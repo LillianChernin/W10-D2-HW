8c0681724c72d09a866c053eb804f5825ea2af39
Lillian Chernin <l.smith1654@gmail.com>
2018-02-13 23:57:36 -0600

    update skills on instructor class

--- codebar.py
+++ codebar.py
@@ -10,9 +10,9 @@ class Student(Member):
         self.reason = reason
 
 class Instructor(Member):
-    def __init__(self,full_name,bio,skills):
+    def __init__(self,full_name,bio):
         Member.__init__(self,full_name)
-        self.skills = skills
+        self.skills = []
         self.bio = bio
     def add_skill(self, skill):
         self.skills.append(skill)
@@ -58,8 +58,11 @@ workshop = Workshop("12/03/2014", "Shutl")
 
 jane = Student("Jane Doe", "I am trying to learn programming and need some help")
 lena = Student("Lena Smith", "I am really excited about learning to program!")
-vicky = Instructor("Vicky Python", "I want to help people learn coding.",["HTML","JavaScript"])
-nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love",["Python"])
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
 
 workshop.add_participant(jane)
 workshop.add_participant(lena)
